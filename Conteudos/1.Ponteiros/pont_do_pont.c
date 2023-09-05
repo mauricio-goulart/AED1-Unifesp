c26b38bd55c6c4b0abcd64ced6442f0a526e2d29
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-09-05 20:27:03 -0300

    Create pont_do_pont.c

---
+++ Conteudos/1.Ponteiros/pont_do_pont.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int *p1 = &a;
+    int **p2 = &p1;
+
+    printf("&a = [%p] a = [%i]\n", &a, a);
+    printf("&p1 = [%p] p1 = [%p] *p1 = [%i]\n", &p1, p1, *p1);
+    printf("&p2 = [%p] p2 = [%p] *p2 = [%p] **p2 = [%i]\n\n", &p2, p2, *p2, **p2);
+
+    **p2 = 99;
+
+    printf("&a = [%p] a = [%i]\n", &a, a);
+    printf("&p1 = [%p] p1 = [%p] *p1 = [%i]\n", &p1, p1, *p1);
+    printf("&p2 = [%p] p2 = [%p] *p2 = [%p] **p2 = [%i]\n\n", &p2, p2, *p2, **p2);
