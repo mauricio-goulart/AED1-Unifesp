a5831d3bb32dcbc3849dc6b3c25ee1944e659a27
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-09-19 22:07:50 -0300

    Create ex.c

---
+++ Conteudos/5.Vetores Dinamicos/ex.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void somar_vetor_com_escalar(int v[], int n, int escalar)
+{
+    for (int c = 0; c < n; c++)
+    {
+        v[c] = v[c] + escalar;
+    }
+    
+}
+
+void print_vetor(int *v, int n)
+{
+    for (int c = 0; c < n; c++)
+    {
+        printf("&v[%d] = [%p] | v[%d] = [%d]\n", c, &v[c], c, v[c]);
+    }
+    
+}
+
+int main()
+{
+    puts("Vetor estatico: ");
+    int v_stack[5] = {0, 3, 4, 5, 6};
+
+    print_vetor(v_stack, 5);
+
+    somar_vetor_com_escalar(v_stack, 5, 10);
+
+    puts("\nVetor dinamico: ");
+    
+    int *v_call = (int *)calloc(5,sizeof(int));
+
+    for (int c = 0; c < 5; c++)
+    {
+        v_call[c] = c *100;
+    }
+
+
+    print_vetor(v_call, 5);
+
+    somar_vetor_com_escalar(v_call, 5, 9);
+
+    puts("");
+
+    print_vetor(v_call, 5);
+
+    free(v_call); 
+
+    return 0;
+}
